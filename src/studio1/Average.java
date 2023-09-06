3d5ef02fe3e2a2ea6f8d4b4ed71c8da4b0b64256
Cal <callhomm@gmail.com>
2023-09-06 13:56:24 -0500

    studio

---
+++ src/studio1/Average.java
@@ -0,0 +1,17 @@
+package studio1;
+
+import java.util.Scanner;
+public class Average {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		//System.out.println("Please input an integer");
+		int a=in.nextInt();
+		//System.out.println("Please input another integer");
+		int b=in.nextInt();
+		System.out.println(((a+b)/2.0));
+		
+
+	}
+
+}
